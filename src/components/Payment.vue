7e479c184e1d54ee411030e2b9f9a587ca7df244
Sergey Rodionov <srodionov@paysend.com>
2021-07-19 20:17:10 +0300

    логи

--- src/components/Payment.vue
+++ src/components/Payment.vue
@@ -84,10 +84,17 @@ export default {
                 currency: this.paymentData.currency,
                 amount: this.paymentData.amount
             })
-            console.log(JSON.stringify(json))
+
+            console.log('json', JSON.stringify(json))
+
             // eslint-disable-next-line
             let encrypted = window.CryptoJS.HmacSHA256(json, this.apiKey.secret)
+
+            console.log('encrypted', JSON.stringify(encrypted))
+
             window.PaysendBuisnessPayment.pay(encrypted);
+
+            console.log('paid');
         },
         loadJS(url, location) {
             var script = document.createElement('script');
