4cb6110aa814b1ceda1093e32f4fdfbdeef7b539
Sergey Rodionov <srodionov@paysend.com>
2021-07-19 23:55:00 +0300

    убрал дублирующий апп

--- src/components/Payment.vue
+++ src/components/Payment.vue
@@ -56,8 +56,8 @@ export default {
         return {
             paymentUrl: "http://157.230.120.240/paysendPaymentLibrary.umd.min.js",
             cryptoUrl: "http://157.230.120.240/crypto-js.js",
-            appUrl: 'http://157.230.120.240/app.173ec3a9735ad9b5d928.js',
-            vendorUrl: 'http://157.230.120.240/vendors.js',
+            // appUrl: 'http://157.230.120.240/app.173ec3a9735ad9b5d928.js',
+            // vendorUrl: 'http://157.230.120.240/vendors.js',
             paymentData: {
                 amount: 1,
                 currency: null,
@@ -111,7 +111,7 @@ export default {
     created() {
         this.loadJS(this.cryptoUrl, document.body);
         this.loadJS(this.paymentUrl, document.body);
-        this.loadJS(this.appUrl, document.body);
+        // this.loadJS(this.appUrl, document.body);
     }
 }
 </script>
