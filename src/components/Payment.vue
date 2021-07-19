aaf6fbe95ec9734532e4c2dd75a7a48ef996487f
Sergey Rodionov <srodionov@paysend.com>
2021-07-19 20:08:07 +0300

    законментил

--- src/components/Payment.vue
+++ src/components/Payment.vue
@@ -102,9 +102,9 @@ export default {
         }
     },
     created() {
-        this.loadJS(this.vendorUrl, document.body);
+        // this.loadJS(this.vendorUrl, document.body);
         this.loadJS(this.cryptoUrl, document.body);
-        this.loadJS(this.paymentUrl, document.body);
+        // this.loadJS(this.paymentUrl, document.body);
         this.loadJS(this.appUrl, document.body);
     }
 }
