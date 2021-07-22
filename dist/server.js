48836d3e65d0960adb47762a83ea38154dcbeecb
Sergey Rodionov <srodionov@paysend.com>
2021-07-23 00:30:42 +0300

    change header

--- dist/server.js
+++ dist/server.js
@@ -1,18 +1,17 @@
 let setCache = function (req, res, next) {
-      // for the other requests set strict no caching parameters
-      res.set('Cache-control', `no-cache, no-store, must-revalidate`)
+    // for the other requests set strict no caching parameters
+    res.set('Cache-control', 'no-cache, no-store, must-revalidate');
 
     // remember to call next() to pass on the request
     next()
-  }
-  
+};
 
-var express = require('express')
-var path = require('path')
-var serveStatic = require('serve-static')
+const express = require('express')
+const path = require('path')
+const serveStatic = require('serve-static')
 app = express()
 app.use(setCache)
 app.use(serveStatic(__dirname))
-var port = process.env.PORT || 5000
+const port = process.env.PORT || 5000
 app.listen(port)
 console.log('server started ' + port)
