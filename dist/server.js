caf888e84199a9097a6800015d89a1b13c13c471
Sergey Rodionov <srodionov@paysend.com>
2021-07-23 00:26:28 +0300

    кэш

--- dist/server.js
+++ dist/server.js
@@ -1,7 +1,17 @@
+let setCache = function (req, res, next) {
+      // for the other requests set strict no caching parameters
+      res.set('Cache-control', `no-cache, no-store, must-revalidate`)
+
+    // remember to call next() to pass on the request
+    next()
+  }
+  
+
 var express = require('express')
 var path = require('path')
 var serveStatic = require('serve-static')
 app = express()
+app.use(setCache)
 app.use(serveStatic(__dirname))
 var port = process.env.PORT || 5000
 app.listen(port)
